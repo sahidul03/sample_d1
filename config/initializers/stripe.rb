120786fcd513ee4e59208b1bddefeb4e4ae6cb36
sahidulislam <sahidul.islam@rootnext.com>
2015-11-09 14:35:09 +0600

    stripe add and css add to print

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,2 +1,6 @@
-Stripe.api_key='sk_test_3thGJMiZWrN1FDfqrkAPUhVM'
-STRIPE_PUBLIC_KEY= 'pk_test_SZALryK25uKP4nBjYXpilPsd'
+Rails.configuration.stripe = {
+    :publishable_key => 'pk_test_SZALryK25uKP4nBjYXpilPsd',
+    :secret_key      => 'sk_test_3thGJMiZWrN1FDfqrkAPUhVM'
+}
+
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
     resources :products
   end
 
-
+  resources :charges
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
